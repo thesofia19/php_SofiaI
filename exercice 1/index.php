397697a860c4ddad8926704f90b4cd54dc5f8655
thesofia19 <73839784+thesofia19@users.noreply.github.com>
2020-12-09 12:03:17 +0100

    Fin de l'exercice 1

--- exercice 1/index.php
+++ exercice 1/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exercice 0</title>
+    <title>exercice 1</title>
 </head>
 <body>
 <h1>Exercice 1</h1>
