c05e7a59e4b690605989d53a21f97380573184f4
thesofia19 <73839784+thesofia19@users.noreply.github.com>
2020-12-10 09:54:30 +0100

    Fin de l'exercice 5

---
+++ exercice 5/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>exercice 5</title>
+    </head>
+
+
+    <body>
+
+        <h1>Exercice 5</h1>
+        <p>
+            Idem que l'exercice 4, mais cette fois-ci avec une boucle "for".
+        </p>
+        <hr>
+
+        <ul>
+            <?php
+
+            for($i = 0; $i < 5000; $i++){
+                echo '<li>'.++$i.'</li>';
+            }
+
+            ?>
+        </ul>
+    </body>
+</html>
